55c21f2b54ba44bd986705429f0661b245e49c1a
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-04-05 11:12:55 -0300

    main manu script organization

--- avancar para reciclar/Scripts/Managers/main_menu.gd
+++ avancar para reciclar/Scripts/Managers/main_menu.gd
@@ -11,41 +11,48 @@ func _input(event: InputEvent) -> void:
 	
 	# dedicate a function to this
 	if Input.is_action_just_pressed("Confirm"):
-		if player1.dice_roll == 0:
-			player1.dice_roll = GameManager.roll_dice(1,6)
-			print("player 1 rolled: " + str(player1.dice_roll))
-			
-		elif player2.dice_roll == 0 || player2.dice_roll == player1.dice_roll:
-			player2.dice_roll = GameManager.roll_dice(1,6)
-			print("player 2 rolled: " + str(player2.dice_roll))
-			
-		elif player3.dice_roll == 0 || player3.dice_roll == player1.dice_roll || player3.dice_roll == player2.dice_roll:
-			player3.dice_roll = GameManager.roll_dice(1,6)
-			print("player 3 rolled: " + str(player3.dice_roll))
+		decide_player_order()
 		
-		else: # when every player rolled a unique number
-			  # dedicate a function to this	
-			var iteration: int = 0
-			
-			# sort player array base on dice roll (ascending)
-			var in_order: bool = false
-			while !in_order:
-				iteration += 1
-				
-				var temp_player
-				# check if it's in order
-				if player_array[0].dice_roll > player_array[1].dice_roll:
-					if player_array[1].dice_roll > player_array[2].dice_roll:
-						in_order = true
-					else: # when player 3 is higher than player 2
-						temp_player = player_array[1]
-						player_array[1] = player_array[2]
-						player_array[2] = temp_player
-				else: # when player 2 is higher than 1
-					temp_player = player_array[0]
-					player_array[0] = player_array[1]
-					player_array[1] = temp_player
-				
-				print("Iteration " + str(iteration))
-			
-			print(player_array[0].dice_roll, player_array[1].dice_roll, player_array[2].dice_roll)
+		
+
+func decide_player_order():
+	if player1.dice_roll == 0:
+		player1.dice_roll = GameManager.roll_dice(1,6)
+		print("player 1 rolled: " + str(player1.dice_roll))
+		
+	elif player2.dice_roll == 0 || player2.dice_roll == player1.dice_roll:
+		player2.dice_roll = GameManager.roll_dice(1,6)
+		print("player 2 rolled: " + str(player2.dice_roll))
+		
+	elif player3.dice_roll == 0 || player3.dice_roll == player1.dice_roll || player3.dice_roll == player2.dice_roll:
+		player3.dice_roll = GameManager.roll_dice(1,6)
+		print("player 3 rolled: " + str(player3.dice_roll))
+	
+	else: # when every player rolled a unique number
+		sort_player_array()
+	
+func sort_player_array():
+	var iteration: int = 0
+	
+	# sort player array base on dice roll (ascending)
+	var in_order: bool = false
+	while !in_order:
+		iteration += 1
+		
+		var temp_player
+		# check if it's in order
+		if player_array[0].dice_roll > player_array[1].dice_roll:
+			if player_array[1].dice_roll > player_array[2].dice_roll:
+				in_order = true
+			else: # when player 3 is higher than player 2
+				temp_player = player_array[1]
+				player_array[1] = player_array[2]
+				player_array[2] = temp_player
+		else: # when player 2 is higher than 1
+			temp_player = player_array[0]
+			player_array[0] = player_array[1]
+			player_array[1] = temp_player
+		
+		print("Iteration " + str(iteration))
+	
+	print(player_array[0].dice_roll, player_array[1].dice_roll, player_array[2].dice_roll)
