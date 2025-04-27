c9ae40a6de9974e074027a1fa6128aed04a963cb
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-04-26 21:08:11 -0300

    added properties to the question card resource

--- avancar para reciclar/Scripts/Cards/question card.gd
+++ avancar para reciclar/Scripts/Cards/question card.gd
@@ -3,3 +3,6 @@ class_name QuestionCard
 
 # maybe this could reference a txt or json file
 @export var card_text: String
+@export var right_alternative: String
+@export var wrong_alternative_1: String
+@export var wrong_alternative_2: String
