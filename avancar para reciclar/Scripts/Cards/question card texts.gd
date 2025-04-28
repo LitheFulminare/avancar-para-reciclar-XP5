99d1d5cde35b60477e1e7e1a13487c2950b8c1d0
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-04-28 14:44:03 -0300

    setup question card resource and scene

---
+++ avancar para reciclar/Scripts/Cards/question card texts.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name QuestionCardTexts
+
+# maybe this could reference a txt or json file
+@export var question: String
+@export var right_answer: String
+@export var wrong_answer_1: String
+@export var wrong_answer_2: String
--- avancar para reciclar/Scripts/Cards/question card.gd
+++ avancar para reciclar/Scripts/Cards/question card.gd
@@ -1,8 +1,18 @@
-extends Resource
 class_name QuestionCard
+extends Node
 
-# maybe this could reference a txt or json file
-@export var card_text: String
-@export var right_alternative: String
-@export var wrong_alternative_1: String
-@export var wrong_alternative_2: String
+# unsure if it's going to be exposed to the editor to asign manually or
+# make an array, preload everything and randomize 1 (I think this option is better)
+#@export var texts: QuestionCardTexts
+var texts: QuestionCardTexts = preload("res://Resources/Cards/Question Cards/test card.tres")
+
+var question: String
+var right_answer: String
+var wrong_answer_1: String
+var wrong_answer_2: String
+
+func _ready() -> void:
+	question = texts.question
+	right_answer = texts.right_answer
+	wrong_answer_1 = texts.wrong_answer_1
+	wrong_answer_2 = texts.wrong_answer_2
