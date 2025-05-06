7a4e3643b33a80568202d53259ad596905f7ba7a
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-05-06 20:32:33 -0300

    preload all resources in the directory
    preloads resources in the question card texts directory

--- avancar para reciclar/Scripts/Managers/question resource manager.gd
+++ avancar para reciclar/Scripts/Managers/question resource manager.gd
@@ -2,3 +2,12 @@
 
 class_name QuestionCardResourceManager
 extends Node
+
+const res_directory_path: String = "res://Resources/Cards/Question Cards/"
+var question_card_texts: Array[QuestionCardTexts]
+
+func _ready() -> void:
+	var res_directory = ResourceLoader.list_directory(res_directory_path)
+		
+	for res in res_directory:
+		question_card_texts.append(load(res_directory_path+res))
