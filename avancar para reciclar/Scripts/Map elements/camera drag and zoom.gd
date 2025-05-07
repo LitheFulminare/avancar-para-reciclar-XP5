4b2b55e695ca3716b95863f2421c36b2ca5ea6aa
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-05-07 01:03:45 -0300

    partial fix camera and zoom going past the limits

--- avancar para reciclar/Scripts/Map elements/camera drag and zoom.gd
+++ avancar para reciclar/Scripts/Map elements/camera drag and zoom.gd
@@ -3,6 +3,8 @@ extends Camera2D
 var zoom_target: Vector2
 @export var pan_speed: float = 300
 
+@export var map_background: Sprite2D
+
 func _ready() -> void:
 	zoom_target = zoom
 
@@ -19,21 +21,30 @@ func zoom_with_mouse(delta: float) -> void:
 		zoom_target *= 0.9
 		
 	zoom = zoom.slerp(zoom_target, 12 * delta)
+	
+	if zoom_target.x < 1:
+		zoom_target.x = 1
+	if zoom_target.y < 1:
+		zoom_target.y = 1
 
 func move_with_keyboard(delta: float) -> void:
 	var movement_direction: Vector2 = Vector2.ZERO
 	
 	if Input.is_action_pressed("Move camera up"):
-		movement_direction.y -= 1
+		if position.y > limit_top:
+			movement_direction.y -= 1
 	if Input.is_action_pressed("Move camera down"):
-		movement_direction.y += 1
+		if position.y < limit_bottom:
+			movement_direction.y += 1
 	if Input.is_action_pressed("Move camera left"):
-		movement_direction.x -= 1
+		if position.x > limit_left:
+			movement_direction.x -= 1
 	if Input.is_action_pressed("Move camera right"):
-		movement_direction.x += 1
+		if position.x < limit_right:
+			movement_direction.x += 1
 		
 	global_position += movement_direction.normalized() * pan_speed * delta
 	
 	# now due to parallax the camera appears to move faster the more it's zoomed in
 	# this prevents that, but it's up to personal preference
-	#global_position += movement_direction.normalized() * pan_speed * delta * 1/zoom.x
+	#global_position += movement_direction.normalized() * pan_speed * delta * (1/zoom.x)
