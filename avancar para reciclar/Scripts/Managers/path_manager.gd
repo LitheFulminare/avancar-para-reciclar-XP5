9a63578a36e332a61960a7726b89a757db5cb0e2
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-05-15 20:01:13 -0300

    setup path manager
    handles branching paths

---
+++ avancar para reciclar/Scripts/Managers/path_manager.gd
@@ -0,0 +1,12 @@
+class_name PathManager
+extends Node
+
+@export_subgroup("Branch 1")
+@export var branch1_B_start: int = 0
+@export var branch1_B_end: int = 0
+@export_subgroup("Branch 2")
+@export var branch2_B_start: int = 0
+@export var branch2_B_end: int = 0
+@export_subgroup("Branch 3")
+@export var branch3_B_start: int = 0
+@export var branch3_B_end: int = 0
