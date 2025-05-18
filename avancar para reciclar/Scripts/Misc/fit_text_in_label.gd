10b3f1c878c8ffbbb327a57ca69fcd7b1bde9d7e
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-05-18 01:17:22 -0300

    progress make text fit label

--- avancar para reciclar/Scripts/Misc/fit_text_in_label.gd
+++ avancar para reciclar/Scripts/Misc/fit_text_in_label.gd
@@ -1,17 +1,21 @@
 class_name LabelManager
 extends Label
 
+var font_theme: Theme = preload("res://Misc/Text/Question card text theme.tres")
+
 var original_height: float
 
 func _ready() -> void:
 	original_height = size.y
 	print("original height: " + str(original_height))
-
+	
 func update_text(new_text: String) -> void:
 	text = new_text
-	#var new_size: float 
+	
+func _on_resized() -> void:
 	print("new size: " + str(size.y))
-	if size.y > original_height:
-		print("original height exceeded")
-	else: 
-		print("original height was not exceeded")
+	while size.y > original_height:
+		add_theme_font_size_override("font_size", get_theme_font_size("font_size")-1)
+		print("new font size: " + str(get_theme_font_size("font_size")-1))
+		print("size after decrease: " + str(size.y))
+		#print(get_theme_font_size("Question card text theme"))
