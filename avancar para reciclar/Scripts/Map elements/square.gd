4ceaab6dcfec643de01440fd1679e275bc83c7cc
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-05-19 19:22:27 -0300

    ajustments to the test map
    added the nodes the game needs to work and started to develop the @tool script on the squares

--- avancar para reciclar/Scripts/Map elements/square.gd
+++ avancar para reciclar/Scripts/Map elements/square.gd
@@ -1,7 +1,13 @@
+#@tool
+
 class_name Square
 extends Node2D
 
 @export var stats: SquareStats
+
+#func _process(delta: float) -> void:
+	#if Engine.is_editor_hint():
+		#return
 
 func action() -> void:
 	get_tree().call_group("Square Manager", "action", stats.type)
