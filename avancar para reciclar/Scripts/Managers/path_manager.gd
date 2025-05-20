67aef1ed051668831d00af5b415a9460894ec050
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-05-20 19:23:30 -0300

    progress storing branching data

--- avancar para reciclar/Scripts/Managers/path_manager.gd
+++ avancar para reciclar/Scripts/Managers/path_manager.gd
@@ -2,11 +2,20 @@ class_name PathManager
 extends Node
 
 @export_subgroup("Branch 1")
+@export var branch1_A_start: int = 0
+@export var branch1_A_end: int = 0
 @export var branch1_B_start: int = 0
 @export var branch1_B_end: int = 0
+
 @export_subgroup("Branch 2")
+@export var branch2_A_start: int = 0
+@export var branch2_A_end: int = 0
 @export var branch2_B_start: int = 0
 @export var branch2_B_end: int = 0
-@export_subgroup("Branch 3")
-@export var branch3_B_start: int = 0
-@export var branch3_B_end: int = 0
+
+var branch_1_A_size: int
+var branch_2_A_size: int
+
+func _ready() -> void:
+	branch_1_A_size = branch1_A_end - branch1_A_start
+	branch_2_A_size = branch2_A_end - branch2_A_start
