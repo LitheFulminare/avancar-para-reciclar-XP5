842fece0272cae44a7e15337fd5c11af498daff1
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-03 20:59:04 -0300

    setup spawn trash randomly during gameplay

--- avancar para reciclar/Scripts/Minigames/Boat/boat minigame manager.gd
+++ avancar para reciclar/Scripts/Minigames/Boat/boat minigame manager.gd
@@ -6,7 +6,13 @@ extends Node
 
 static var ignore_input  = false
 
+# used to store trash scenes and instantiate them during gameplay
+const trash_directory: String = "res://Scenes/Minigames/Trash/"
+var trash_pool: Array[PackedScene]
+
 func _ready() -> void:
+	load_trash()
+	
 	trash_parent.child_exiting_tree.connect(trash_collected)
 	timer_clock.timer_reached_zero.connect(timer_ended)
 
@@ -20,3 +26,10 @@ func trash_collected(_node: Node) -> void:
 # called when the timer on the Timer Clock reaches 0
 func timer_ended() -> void:
 	ignore_input = true
+
+# called on ready(), used to get all "Trash" scenes and put them on an array
+func load_trash() -> void:
+	var trash_scenes = ResourceLoader.list_directory(trash_directory)
+	
+	for scene in trash_scenes:
+		trash_pool.append(load(trash_directory+scene))
