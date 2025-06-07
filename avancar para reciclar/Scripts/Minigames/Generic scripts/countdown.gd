b3dc03e933b9b8aad82332edc08a9dbfc42b3633
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-07 20:35:44 -0300

    fixed the text's pivot offset

--- avancar para reciclar/Scripts/Minigames/Generic scripts/countdown.gd
+++ avancar para reciclar/Scripts/Minigames/Generic scripts/countdown.gd
@@ -17,41 +17,49 @@ func start_countdown() -> void:
 	countdown_text.scale = Vector2.ZERO
 	
 	await get_tree().create_timer(1).timeout
-	tween_label_scale()
 	countdown_text.text = "3"
+	tween_label_scale()
 	
 	await get_tree().create_timer(0.7).timeout
-	countdown_text.text = ""
 	countdown_text.scale = Vector2.ZERO
+	countdown_text.text = ""
 	
 	await get_tree().create_timer(0.3).timeout
-	tween_label_scale()
 	countdown_text.text = "2"
+	tween_label_scale()
 	
 	await get_tree().create_timer(0.7).timeout
-	countdown_text.text = ""
 	countdown_text.scale = Vector2.ZERO
+	countdown_text.text = ""
 	
 	await get_tree().create_timer(0.3).timeout
-	tween_label_scale()
 	countdown_text.text = "1"
+	tween_label_scale()
 	
 	await get_tree().create_timer(0.7).timeout
-	countdown_text.text = ""
 	countdown_text.scale = Vector2.ZERO
+	countdown_text.text = ""
 	
 	await get_tree().create_timer(0.3).timeout
-	tween_label_scale()
 	countdown_text.text = "Já!"
+	tween_label_scale()
 	
 	await get_tree().create_timer(0.7).timeout
-	countdown_text.text = ""
 	countdown_text.scale = Vector2(1, 1)
+	countdown_text.text = ""
 	
 	await get_tree().create_timer(0.3).timeout
 	
 	countdown_ended.emit()
 	
 func tween_label_scale() -> void:
+	# wait for the label size to be changed before updating the pivot offset
+	await get_tree().process_frame
+	
+	update_offset()
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(countdown_text, "scale", Vector2(1, 1), 0.15)
+
+func update_offset() -> void:
+	countdown_text.pivot_offset.x = countdown_text.size.x / 2
+	countdown_text.pivot_offset.y = countdown_text.size.y / 2
