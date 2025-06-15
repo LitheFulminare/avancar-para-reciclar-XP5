3d302a6d24d84b3307665732c217f48b5765f65d
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-15 01:38:55 -0300

    finished display whose turn it is

--- avancar para reciclar/Scripts/HUD/game_message_manager.gd
+++ avancar para reciclar/Scripts/HUD/game_message_manager.gd
@@ -3,4 +3,33 @@
 class_name GameMessageManager
 extends Label
 
+enum players
+{
+	green,
+	yellow,
+	red
+}
+
 @export var seconday_label: Label
+
+@export var green_color: Color
+@export var yellow_color: Color
+@export var red_color: Color
+
+func display_current_player(current_player: players) -> void:
+	var player_color_name: String
+	var new_color: Color
+	
+	match current_player:
+		players.green:
+			new_color = green_color
+			player_color_name = "VERDE"
+		players.yellow:
+			new_color = yellow_color
+			player_color_name = "AMARELO"
+		players.red:
+			new_color = red_color
+			player_color_name = "VERMELHO"
+			
+	seconday_label.text = "\n" + player_color_name
+	seconday_label.add_theme_color_override("font_color", new_color)
