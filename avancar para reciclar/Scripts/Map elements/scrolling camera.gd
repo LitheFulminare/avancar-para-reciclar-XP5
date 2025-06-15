e1cf793aa96734490a74b2b88e4cb7b27251fed7
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-15 18:55:13 -0300

    fixed camera not zooming in

--- avancar para reciclar/Scripts/Map elements/scrolling camera.gd
+++ avancar para reciclar/Scripts/Map elements/scrolling camera.gd
@@ -29,18 +29,15 @@ func _process(delta: float) -> void:
 	
 	
 func zoom_in_and_out(delta: float) -> void:
-	if ignore_input:
-		return
-
 	# is_action_pressed doesn't work with scroll wheel, but I want it to be possible to hold the zoom button
 	if Input.is_action_pressed("Camera zoom in") || Input.is_action_just_pressed("Camera zoom in"):
-		if !is_zoom_on_cooldown:
+		if !is_zoom_on_cooldown && !ignore_input:
 			zoom_cooldown_timer.start()
 			is_zoom_on_cooldown = true
 			zoom_target *= 1.1
 		
 	if Input.is_action_pressed("Camera zoom out") || Input.is_action_pressed("Camera zoom out"):
-		if !is_zoom_on_cooldown:
+		if !is_zoom_on_cooldown && !ignore_input:
 			zoom_cooldown_timer.start()
 			is_zoom_on_cooldown = true
 			zoom_target *= 0.9
