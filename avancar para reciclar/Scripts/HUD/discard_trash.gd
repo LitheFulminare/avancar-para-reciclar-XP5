4e85a8703a975a85c9ec6b824189b0bbea54427a
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-23 22:00:35 -0300

    setup spawn discard trash scene
    also setup the "no trash" warning

--- avancar para reciclar/Scripts/HUD/discard_trash.gd
+++ avancar para reciclar/Scripts/HUD/discard_trash.gd
@@ -1,2 +1,13 @@
 class_name DiscardTrash
 extends Node2D
+
+@export var warning_label: Label
+
+func _ready() -> void:
+	warning_label.modulate.a = 0
+
+func spawn_no_trash_warning() -> void:
+	if warning_label.modulate.a == 0:
+		warning_label.modulate.a = 1
+		var tween: Tween = get_tree().create_tween()
+		tween.tween_property(warning_label, "modulate:a", 0, 2)
--- avancar para reciclar/Scripts/Managers/discard_trash_manager.gd
+++ avancar para reciclar/Scripts/Managers/discard_trash_manager.gd
@@ -4,5 +4,8 @@ extends Node
 @export var round_manager: RoundManager
 @export var discard_trash_scene: PackedScene
 
+var discard_trash: DiscardTrash
+
 func discard_any_trash() -> void:
-	discard_trash_scene.instantiate()
+	discard_trash = discard_trash_scene.instantiate()
+	add_child(discard_trash)
