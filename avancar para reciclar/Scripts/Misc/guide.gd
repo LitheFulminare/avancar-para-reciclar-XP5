852272b3912bb0b9d2760bbbff2e7e67ff5ac374
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-24 01:01:48 -0300

    finished setting up guide screen

--- avancar para reciclar/Scripts/Misc/guide.gd
+++ avancar para reciclar/Scripts/Misc/guide.gd
@@ -1,5 +1,16 @@
 class_name Guide
 extends Node2D
 
+@export var guide_elements: Control
+@export var show_guide_button: TextureButton
+
+func _ready() -> void:
+	guide_elements.visible = false
+
 func _on_continue_button_pressed() -> void:
-	visible = false
+	guide_elements.visible = false
+	show_guide_button.visible = true
+
+func _on_show_guide_button_pressed() -> void:
+	show_guide_button.visible = false
+	guide_elements.visible = true
