1f17f403c6f73804b39f27b72a3e6053e97699ac
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2025-06-23 22:17:20 -0300

    connected signals

--- avancar para reciclar/Scripts/HUD/discard_trash.gd
+++ avancar para reciclar/Scripts/HUD/discard_trash.gd
@@ -1,6 +1,12 @@
 class_name DiscardTrash
 extends Node2D
 
+signal glass_button_pressed
+signal metal_button_pressed
+signal organic_button_pressed
+signal paper_button_pressed
+signal plastic_button_pressed
+
 @export var warning_label: Label
 
 func _ready() -> void:
@@ -16,3 +22,19 @@ func spawn_no_trash_warning() -> void:
 		warning_label.modulate.a = 1
 		var tween: Tween = get_tree().create_tween()
 		tween.tween_property(warning_label, "modulate:a", 0, 5)
+
+
+func _on_glass_pressed() -> void:
+	glass_button_pressed.emit()
+
+func _on_metal_pressed() -> void:
+	metal_button_pressed.emit()
+
+func _on_organic_pressed() -> void:
+	organic_button_pressed.emit()
+
+func _on_paper_pressed() -> void:
+	paper_button_pressed.emit()
+
+func _on_plastic_pressed() -> void:
+	plastic_button_pressed.emit()
--- avancar para reciclar/Scripts/Managers/discard_trash_manager.gd
+++ avancar para reciclar/Scripts/Managers/discard_trash_manager.gd
@@ -10,6 +10,30 @@ func _ready() -> void:
 	discard_trash = discard_trash_scene.instantiate()
 	add_child(discard_trash)
 	discard_trash.visible = false
+	
+	connect_signals()
+
+func connect_signals() -> void:
+	discard_trash.glass_button_pressed.connect(_on_glass_pressed)
+	discard_trash.metal_button_pressed.connect(_on_metal_pressed)
+	discard_trash.organic_button_pressed.connect(_on_organic_pressed)
+	discard_trash.paper_button_pressed.connect(_on_paper_pressed)
+	discard_trash.plastic_button_pressed.connect(_on_plastic_pressed)
 
 func discard_any_trash() -> void:
 	discard_trash.visible = true
+
+func _on_glass_pressed() -> void:
+	print("Glass pressed")
+
+func _on_metal_pressed() -> void:
+	print("Metal pressed")
+	
+func _on_organic_pressed() -> void:
+	print("Organic pressed")
+	
+func _on_paper_pressed() -> void:
+	print("Paper pressed")
+	
+func _on_plastic_pressed() -> void:
+	print("Plastic pressed")
